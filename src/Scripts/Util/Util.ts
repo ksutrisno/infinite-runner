c2ffa6e5bba1c2f2162b118702530177ab16544e
ksutrisno <kevinjsutrisno@gmail.com>
2021-11-18 20:41:06 +0700

    update

--- src/Scripts/Util/Util.ts
+++ src/Scripts/Util/Util.ts
@@ -1,11 +1,15 @@
 export const getResolution = () => {
   
-
-
-     
-          var w = window.innerWidth;
-          var h = window.innerHeight;
-     
+    let isMobile = navigator.userAgent.indexOf("Mobile");
+      if (isMobile == -1) {
+          isMobile = navigator.userAgent.indexOf("Tablet");
+      }
+      let w = 720;
+      let h = 1200;
+      if (isMobile !== -1) {
+          w = window.innerWidth;
+          h = window.innerHeight;
+      }
   
     return { width: w, height: h };
   };
